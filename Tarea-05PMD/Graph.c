092cef627d9f8929d0f7027d475a2cc7a852acca
OmarSP99 <39529847+OmarSP99@users.noreply.github.com>
2018-07-05 14:18:29 -0500

    Update Graph.c

--- Tarea-05PMD/Graph.c
+++ Tarea-05PMD/Graph.c
@@ -1,3 +1,28 @@
 //Directivas al preprocesador
 #include <stdio.h>
 #include <stdlib.h>
+
+
+struct node
+{
+	int id;
+	Type data;
+}strNode;
+
+typedef struct strNode Node;
+
+struct grph
+{
+	int* arry=0;
+	int Nvertex;
+	int Nedge;
+}strGraph;
+
+unsigned long graph_vertexCount(Graph g)
+{
+	return g->Nvertex;
+}
+unsigned long graph_edgeCount(Graph g)
+{
+	return g->Nedge;
+}
