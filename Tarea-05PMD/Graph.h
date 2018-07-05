e27111a1c53354ce60329b06c34a39d9760a50a8
OmarSP99 <39529847+OmarSP99@users.noreply.github.com>
2018-07-05 14:19:11 -0500

    Update Graph.h

--- Tarea-05PMD/Graph.h
+++ Tarea-05PMD/Graph.h
@@ -8,6 +8,18 @@
 #ifndef GRAPH_H_
 #define GRAPH_H_
 
+typedef void* Type;
+typedef struct strGraph Graph;
+typedef enum {true, flase}boolean;
 
+Graph graph_create(CMP comparator);
+void graph_destroy(Graph g);
+boolean graph_addVertex(Graph g, Type data);
+boolean graph_addEdge(Graph g, unsigned long source, unsigned long sink);
+unsigned long graph_vertexCount(Graph g);
+unsigned long graph_edgeCount(Graph g);
+unsigned long graph_outDegree(Graph g, unsigned long source);
+boolean graph_hasEdge(Graph g, unsigned long source, unsigned long sink);
+boolean graph_print(Graph g, Print p);
 
 #endif /* GRAPH_H_ */
