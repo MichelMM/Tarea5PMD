8c741c79562c75225c4107fca20c263fee68256e
Ignacio Itamar Vazquez Martinez <39530036+ignacio-git@users.noreply.github.com>
2018-07-05 15:13:43 -0500

    Update Graph.c

--- Tarea-05PMD/Graph.c
+++ Tarea-05PMD/Graph.c
@@ -2,27 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-struct node
+typedef struct strNode
 {
     int id;
     Type data;
-}strNode;
+    struct strNode *peek;
+};
 
 typedef struct strNode Node;
 
-struct graph
+struct strGraph
 {
-    int* arry=0;
-    int Nvertex;
-    int Nedge;
+    Node *first;
+    int num_vertices;
+    int num_edges;
 }strGraph;
 
-unsigned long graph_vertexCount(Graph g)
-{
-    return g->Nvertex;
+//unsigned long graph_vertexCount(Graph g)
+//{
+//    return g->Nvertex;
+//}
+//unsigned long graph_edgeCount(Graph g)
+//{
+//    return g->Nedge;
+//}
+
+Graph *graph_create(CMP comparator){
+    Graph *new = (Graph*)malloc(sizeof(struct strGraph));
+    (*new).first = NULL;
+    (*new).num_edges = 0;
+    (*new).num_vertex =0;
+    return new;
 }
-unsigned long graph_edgeCount(Graph g)
-{
-    return g->Nedge;
+boolean graph_addVertex(Graph graph, Type data){
+    Node *new = (Node*)malloc(sizeof(struct strNode));
+    if((*graph).num_vertices==0){
+        (*new).data = data;
+        (*new).id =
+    }
 }
