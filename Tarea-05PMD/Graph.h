5e1bc5bf786603fcdba0b8b57bad496870d55f27
Ignacio Itamar Vazquez Martinez <39530036+ignacio-git@users.noreply.github.com>
2018-07-05 15:16:30 -0500

    Update Graph.h

--- Tarea-05PMD/Graph.h
+++ Tarea-05PMD/Graph.h
@@ -10,17 +10,23 @@
 
 typedef void* Type;
 typedef struct strGraph Graph;
-typedef enum {true, flase}boolean;
+typedef enum {true, false}boolean;
 typedef void (*CMP)(void*);
 typedef void (*Print)(void*);
 
-Graph graph_create(CMP comparator);
-void graph_destroy(Graph g);
-boolean graph_addVertex(Graph g, Type data);
-boolean graph_addEdge(Graph g, unsigned long source, unsigned long sink);
-unsigned long graph_vertexCount(Graph g);
-unsigned long graph_edgeCount(Graph g);
-unsigned long graph_outDegree(Graph g, unsigned long source);
-boolean graph_hasEdge(Graph g, unsigned long source, unsigned long sink);
-boolean graph_print(Graph g, Print p);
+Graph *graph_create(CMP comparator);
+
+
+
+
+void graph_destroy(Graph graph);
+boolean graph_addVertex(Graph graph, Type data);
+boolean graph_addEdge(Graph graph, unsigned long source, unsigned long sink);
+unsigned long graph_vertexCount(Graph graph);
+unsigned long graph_edgeCount(Graph graph);
+unsigned long graph_outDegree(Graph graph, unsigned long source);
+boolean graph_hasEdge(Graph graph, unsigned long source, unsigned long sink);
+boolean graph_print(Graph graph, Print p);
+
+
 #endif /* GRAPH_H_ */
