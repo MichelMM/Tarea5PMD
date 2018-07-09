7193b522d12123bc76aad83511af4a041e4bd945
Michel Maris Mora <39529432+MichelMM@users.noreply.github.com>
2018-07-09 17:14:48 -0500

    Update Graph.c

--- Tarea-05PMD/Graph.c
+++ Tarea-05PMD/Graph.c
@@ -17,13 +17,11 @@ struct strList {
 	unsigned int size;
 };
 
-
 struct strNodeG {
 	int id;
 	Type data;
 	List vertex_list;
 };
-
 
 typedef struct strNodeG NodeG;
 
@@ -35,140 +33,131 @@ struct strGraph {
 	Print printFunction;
 };
 
-
-
-
-
 /*--------------------------------------*/
 /*              List                    */
 /*--------------------------------------*/
 
-List list_create(){
+List list_create() {
 	List l;
-	l=(List)malloc(sizeof(struct strList));
-	l->size=0;
-	l->last=NULL;
-	l->first=NULL;
+	l = (List) malloc(sizeof(struct strList));
+	l->size = 0;
+	l->last = NULL;
+	l->first = NULL;
 	return l;
 }
 
-int list_size(List l){
-	if(l!=NULL)
+int list_size(List l) {
+	if (l != NULL)
 		return l->size;
 	else
 		return -1;
 }
 
-NodeL * createNode(Type data){
+NodeL * createNode(Type data) {
 	NodeL *new;
-	new=(NodeL *)malloc(sizeof(NodeL));
-	new->Data=(void *)malloc(sizeof(*data));
+	new = (NodeL *) malloc(sizeof(NodeL));
+	new->Data = (void *) malloc(sizeof(*data));
 	//memcpy(new->Data, data, sizeof(*data));
-	new->next=NULL;
-	new->prior=NULL;
+	new->next = NULL;
+	new->prior = NULL;
 	return new;
 }
 
-void list_add(List l, Type data){
+void list_add(List l, Type data) {
 	//Agrega un elemento al final de la lista
 	//e incrementa el size
-	if(l!=NULL){
+	if (l != NULL) {
 		NodeL *new;
-		new=createNode(data);
-		if(l->size==0)
-		{
-			l->first=new;
-			l->last=new;
-		}
-		else{
-			l->last->next=new;
-			new->prior=l->last;
-			l->last=new;
+		new = createNode(data);
+		if (l->size == 0) {
+			l->first = new;
+			l->last = new;
+		} else {
+			l->last->next = new;
+			new->prior = l->last;
+			l->last = new;
 		}
 		l->size++;
 	}
 }
 
-Type list_get(List l, int p){
-	Type value=NULL;
-	NodeL *current=NULL;
-	int i=0;
-	int s=l->size;
-	if(l!=NULL){
-		if ((p>=0) && (p<s)){
-			current=l->first;
-			while(i<p){
-				current=current->next;
+Type list_get(List l, int p) {
+	Type value = NULL;
+	NodeL *current = NULL;
+	int i = 0;
+	int s = l->size;
+	if (l != NULL) {
+		if ((p >= 0) && (p < s)) {
+			current = l->first;
+			while (i < p) {
+				current = current->next;
 				i++;
 			}
-			value=current->Data;
+			value = current->Data;
 		}
 	}
 	return value;
 }
 
-void list_set(List l, Type data, int p){
-	NodeL *current=NULL;
-	int i=0;
-	int s=l->size;
-	if(l!=NULL){
-		if ((p>=0) && (p<s)){
-			current=l->first;
-			while(i<p){
-				current=current->next;
+void list_set(List l, Type data, int p) {
+	NodeL *current = NULL;
+	int i = 0;
+	int s = l->size;
+	if (l != NULL) {
+		if ((p >= 0) && (p < s)) {
+			current = l->first;
+			while (i < p) {
+				current = current->next;
 				i++;
 			}
-			current->Data=data;
+			current->Data = data;
 		}
 	}
 }
 
-Type list_remove(List l, int p){
-	NodeL *current=NULL;
+Type list_remove(List l, int p) {
+	NodeL *current = NULL;
 	Type tmp;
-	int i=0;
-	int s=l->size;
-	if(l!=NULL){
+	int i = 0;
+	int s = l->size;
+	if (l != NULL) {
 		//Buscar el nodo a remover
-		if ((p>=0) && (p<s)){
-			current=l->first;
-			while(i<p){
-				current=current->next;
+		if ((p >= 0) && (p < s)) {
+			current = l->first;
+			while (i < p) {
+				current = current->next;
 				i++;
 			}
 		}
-		if((p==0)&&(s==1)){
-			tmp=current->Data;
-			l->first=NULL;
-			l->last=NULL;
-			l->size=0;
+		if ((p == 0) && (s == 1)) {
+			tmp = current->Data;
+			l->first = NULL;
+			l->last = NULL;
+			l->size = 0;
 			free(current);
 			return tmp;
-		}else{
-			if(p==0) //Se va a eliminar el nodo inicial
-			{
-				tmp=current->Data;
-				l->first=current->next;
-				l->first->prior=NULL;
+		} else {
+			if (p == 0) //Se va a eliminar el nodo inicial
+					{
+				tmp = current->Data;
+				l->first = current->next;
+				l->first->prior = NULL;
 				l->size--;
 				free(current);
 				return tmp;
-			}
-			else{
-				if(p==(s-1))//Se va a eliminar el nodo final
-				{
-					tmp=current->Data;
-					l->last=current->prior;
-					l->last->next=NULL;
+			} else {
+				if (p == (s - 1)) //Se va a eliminar el nodo final
+						{
+					tmp = current->Data;
+					l->last = current->prior;
+					l->last->next = NULL;
 					l->size--;
 					free(current);
 					return tmp;
-				}
-				else
-				{//El elemento a remover esta en medio
-					tmp=current->Data;
-					current->prior->next=current->next;
-					current->next->prior=current->prior;
+				} else { //El elemento a remover esta en medio
+					tmp = current->Data;
+					current->prior->next = current->next;
+					current->next->prior = current->prior;
 					free(current);
 					return tmp;
 				}
@@ -179,8 +168,8 @@ Type list_remove(List l, int p){
 	return NULL;
 }
 
-void list_destroy(List l){
-	while(list_size(l)>0)
+void list_destroy(List l) {
+	while (list_size(l) > 0)
 		list_remove(l, 0);
 	free(l);
 }
@@ -189,7 +178,7 @@ void list_destroy(List l){
 /*              Graph                   */
 /*--------------------------------------*/
 
-Graph graph_create(CMP comparator,Print print) {
+Graph graph_create(CMP comparator, Print print) {
 	Graph new = (Graph) malloc(sizeof(struct strGraph));
 	new->array = NULL;
 	new->num_edges = 0;
@@ -202,54 +191,57 @@ Graph graph_create(CMP comparator,Print print) {
 boolean graph_addVertex(Graph graph, Type data) {
 	NodeG *new = (NodeG*) malloc(sizeof(struct strNodeG));
 	graph->num_vertex++;
-	int size =graph->num_vertex;
-	graph->array = (NodeG*)realloc(graph->array,sizeof(struct strNodeG)*size);
+	int size = graph->num_vertex;
+	graph->array = (NodeG*) realloc(graph->array,
+			sizeof(struct strNodeG) * size);
 	new->data = data;
 	(*new).id = size;
 	List newL;
-	newL= list_create();
+	newL = list_create();
 	new->vertex_list = newL;
 //	graph->array[graph->num_vertex]=new;
-	((*graph).array[size])=*new;
+	((*graph).array[size]) = *new;
 	return true;
 }
 
-boolean graph_addEdge(Graph graph, Type source, Type sink){
-	int flag;
-	Type *compare;
-
-	for(int i=0;i<graph->num_vertex;i++){
-//		*compare = *(graph->array[i]->data);
-		graph-> cmpFunction(graph->array[i]->data,source);
-		if(source== || sink==compare){
+boolean graph_addEdge(Graph graph, Type source, Type sink) {
+	int flag = 0;
+	NodeG *Sink;
+	NodeG *Source;
+	for (int i = 0; i < graph->num_vertex; i++) {
+		//        *compare = *(graph->array[i]->data);
+		if (graph->cmpFunction(graph->array[i].data, source) == true
+				|| graph->cmpFunction(graph->array[i].data, sink) == true) {
+			if (graph->cmpFunction(graph->array[i].data, sink) == true) {
+				Sink = &graph->array[i];
+			}
+			if (graph->cmpFunction(graph->array[i].data, source) == true) {
+				Source = &graph->array[i];
+			}
 			flag++;
 		}
-
 	}
-
-
-
-
-
-
-//
-//	while(){
-//
-//	}
-//
-//	list_add(graph->array[source]->vertex_list,graph->array[sink]);
-//	return true;
+	if (flag == 2) {
+		for (int i = 0; i < Source->vertex_list->size; i++) {
+			if (Sink == list_get(Source->vertex_list, i)) {
+				return false;
+			}
+			list_add(Source->vertex_list, Sink);
+			return true;
+		}
+	}
+	return false;
 }
 
-
-void graph_destroy(Graph graph){
-	int i=0;
-	while(graph->num_vertex != 0)
-	{
+void graph_destroy(Graph graph) {
+	int i = 0;
+	while (graph->num_vertex != 0) {
 		list_destroy(graph->array[i].vertex_list);
-		free(graph->array[i]);
 		graph->num_vertex--;
 		i++;
 	}
+	free(graph->array);
 	free(graph);
+
 }
+
