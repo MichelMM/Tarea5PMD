58b41ba6b1b0a7c844b30e1ce8480b9f5cead6e2
Michel Maris Mora <39529432+MichelMM@users.noreply.github.com>
2018-07-10 14:38:18 -0500

    Update Graph.h

--- Tarea-05PMD/Graph.h
+++ Tarea-05PMD/Graph.h
@@ -7,8 +7,9 @@ typedef struct	 strList *List;
 typedef enum {true, false}boolean;
 typedef int (*CMP)(void *, void *);
 typedef void (*Print)(void*);
+typedef Type (*Clone) (Type);
 
-Graph graph_create(CMP comparator, Print print);//Crea una nueva instancia del contenedor Graph y retorna su dirección de memoria.
+Graph graph_create(CMP comparator, Print print, Clone clonar);//Crea una nueva instancia del contenedor Graph y retorna su dirección de memoria.
 void graph_destroy(Graph graph);//Destruye la instancia del contenedor Graph.
 boolean graph_addVertex(Graph graph, Type data);//Agrega un nuevo vértice al grafo de manera ordenada
 boolean graph_addEdge(Graph graph, Type source, Type sink);//Agrega una nueva arista al grafo
@@ -16,7 +17,7 @@ unsigned long graph_vertexCount(Graph graph);//Retorna el número de vértices q
 unsigned long graph_edgeCount(Graph graph);//Retorna el número de aristas que existen en el grafo.
 unsigned long graph_outDegree(Graph graph,Type source);//Retorna el número de vértices sucesores que tiene en su lista el vértice
 boolean graph_hasEdge(Graph graph, Type source, Type sink);//Retorna True si el vértice del grafo que contiene source tiene en su lista de sucesores un vértice que contiene sink.
-boolean graph_print(Graph graph);//Imprime todos los datos almacenados en el grafo.
+void graph_print(Graph graph);//Imprime todos los datos almacenados en el grafo.
 
 List list_create();//Crea un ejemplar List con memoria dinámica y asigna NULL, NULL y 0 a sus elementos.
 int list_size(List l);//Elimina a todos los nodos y a la lista.
